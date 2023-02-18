f908f2990f7da7abf08fcb0f53e95715f251c326
daichi0918 <kayasugadaichi@icloud.com>
2023-02-18 10:59:37 +0900

    create:header component

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -6,28 +6,9 @@ import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
 import { Link } from 'react-router-dom'
+import { Header } from '../components/header'
 
 const { useState } = React
-
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  justify-content: space-between;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`
-
-const HeaderControl = styled.div`
-    height: 2rem;
-    display: flex;
-    align-content: center;
-  `
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -37,27 +18,40 @@ const Wrapper = styled.div`
   top: 3rem;
 `
 
-const TextArea = styled.textarea`
-  border-right: 1px solid silver;
-  border-top: 1px solid silver;
-  bottom: 0;
-  font-size: 1rem;
-  left: 0;
-  padding: 0.5rem;
-  position: absolute;
+const HeaderControl = styled.div`
+    height: 2rem;
+    display: flex;
+    align-content: center;
+  `
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
   top: 0;
-  width: 50vw;
+  left: 0;
+`
+
+const TextArea = styled.textarea`
+border - right: 1px solid silver;
+border - top: 1px solid silver;
+bottom: 0;
+font - size: 1rem;
+left: 0;
+padding: 0.5rem;
+position: absolute;
+top: 0;
+width: 50vw;
 `
 
 const Preview = styled.div`
-  border-top: 1px solid silver;
-  bottom: 0;
-  overflow-y: scroll;
-  padding: 1rem;
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: 50vw;
+border - top: 1px solid silver;
+bottom: 0;
+overflow - y: scroll;
+padding: 1rem;
+position: absolute;
+right: 0;
+top: 0;
+width: 50vw;
 `
 
 const StorageKey = 'pages/editor:text'
@@ -69,17 +63,16 @@ export const Editor: React.FC = () => {
 
   return (
     <>
-      <Header>
-        Markdown Editor
-        <HeaderControl>
+      <HeaderArea>
+        <Header title="Markdown Editor">
           <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
           <Link to="/history">
             履歴を見る
           </Link>
-        </HeaderControl>
-      </Header>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea
           onChange={(event) => setText(event.target.value)}
